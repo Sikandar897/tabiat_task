0be906bb72add85e744b822ad1da66960f24fe99
Sikandar897 <sikandarafridi6@gmail.com>
2025-10-20 21:06:50 +0500

    add: added task binding to initlizie controller when needed

--- lib/app/bindings/task_binding.dart
+++ lib/app/bindings/task_binding.dart
@@ -1,9 +1,10 @@
-import 'package:get/get.dart';
+
+
+import '../../src.dart';
 
 class TaskBinding extends Bindings {
   @override
   void dependencies() {
-    // Using lazyPut so controller is only created when needed.
     Get.lazyPut<TaskController>(() => TaskController());
   }
 }
